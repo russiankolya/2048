0df35b0915c3114e0d0875c2b8ec795e1cb2c20a
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-01-05 21:30:57 +0400

    Initial commit

---
+++ src/Game.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <AppCore/AppCore.h>
+
+using namespace ultralight;
+
+class Game : public AppListener,
+             public WindowListener,
+             public LoadListener,
+             public ViewListener {
+public:
+    Game();
+
+    ~Game() override;
+
+    // Start the run loop.
+    virtual void Run();
+
+    // This is called continuously from the app's main loop.
+    void OnUpdate() override;
+
+    // This is called when the window is closing.
+    void OnClose(ultralight::Window *window) override;
+
+    // This is called whenever the window resizes.
+    void OnResize(ultralight::Window *window, uint32_t width, uint32_t height) override;
+
+    // This is called when the page finishes a load in one of its frames.
+    void OnFinishLoading(ultralight::View *caller,
+                         uint64_t frame_id,
+                         bool is_main_frame,
+                         const String &url) override;
+
+    // This is called when the DOM has loaded in one of its frames.
+    void OnDOMReady(ultralight::View *caller,
+                    uint64_t frame_id,
+                    bool is_main_frame,
+                    const String &url) override;
+
+    // This is called when the page requests to change the Cursor.
+    void OnChangeCursor(ultralight::View *caller,
+                        Cursor cursor) override;
+
+    void OnChangeTitle(ultralight::View *caller,
+                       const String &title) override;
+
+protected:
+    RefPtr<App> app_;
+    RefPtr<Window> window_;
+    RefPtr<Overlay> overlay_;
+};
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "Game.h"
+
+int main() {
+    Game app;
+    app.Run();
+
+    return 0;
+}
