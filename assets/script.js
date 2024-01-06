392849121c9fc73a520efde93cc17048f52d57b0
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-01-06 20:40:25 +0400

    Add board html structure and some styles

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
-function hello(s) {
-    document.getElementById("hello").innerHTML = s
-}
+const SIZE = 4
 
-hello("Hello from JS")
+let board = document.getElementById("board")
+
+for (let i = 0; i < SIZE; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "board-row");
+
+    for (let j = 0; j < SIZE; j++) {
+        let tile = document.createElement("div");
+
+        let tile_id = "tile" + i + j;
+
+        tile.setAttribute("id", tile_id);
+        tile.setAttribute("class", "tile");
+
+        row.appendChild(tile);
+    }
+    board.appendChild(row);
+}
